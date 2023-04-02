498c1a4f2beac85a94072a8d2fcaec17f9ffb47c
Zakir Ufuk Sahiner <z.ufuk.sahiner@gmail.com>
2023-04-02 23:32:40 +0200

    Added timer to display remaining time after user selects egg hardness, added stop button to stop alarm when eggs are ready (also resets every setting (progress bar, variables, timer etc) and return backs to starting point), added flash effect it turns on when the eggs are ready, and some bug fixes which I discovered later on. Now the app is working perfectly with more functions

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,9 +11,9 @@ import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
-    
-    @IBOutlet weak var progressBar: UIProgressView! 
-    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var stopTimerButton: UIButton!
+    @IBOutlet weak var timeLeft: UILabel!
     
     /* Alternative way
      let softTime = 5
@@ -22,72 +22,122 @@ class ViewController: UIViewController {
      */
     // With using Dictionary
     let eggTimes = ["Soft": 10, "Medium": 7, "Hard": 5] //Named same as Buttons so it matches automatically
-        // 5 7 12
-    var counter: Int = 0
+    // 5 7 12
+    var totalTime: Int = 0
     var secondsPassed = 0
     var eggType: String = ""
+    var timer = Timer()
+    var player: AVAudioPlayer!
     
     
-    // **************************** TIMER SETTINGS ******************
+    // ********************** HOW THE PROGRAM LOADS / START UP SETTINGS **********************
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timeLeft.isHidden = true
+        titleLabel.font = .boldSystemFont(ofSize: 30)
+        stopTimerButton.isHidden = true
+        stopTimerButton.layer.cornerRadius = 15
+        timeLeft.font = .boldSystemFont(ofSize: 40)
+        progressBar.setProgress(0, animated: false)
+        // rounded edges for progressBar
+        progressBar.layer.cornerRadius = 11
+        progressBar.clipsToBounds = true
     }
     
-    // *************************** ALARM SOUND ************************
-    var player: AVAudioPlayer?
-
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    
-    
-    
-    
-    
-    
-    // ********************** PROGRESS BAR ****************************
-    
-    @objc func updateCounter() {
-        //updating progress bar
-        if secondsPassed <= counter {
-            let percentageProgress = Float(secondsPassed) / Float(counter)
-            secondsPassed += 1
-            progressBar.progress = percentageProgress
-            
-        } else {
-            titleLabel.text = "Done!"
-            playSound()
-        }
-    }
-    
-    // *********************** WHEN USER PRESSES BUTTON ************************
+    // ********************** WHEN USER SELECTS HARDNESS **********************
     
     @IBAction func hardnessSelect(_ sender: UIButton) {
-        let hardness = sender.currentTitle! // returns; Soft, Medium or Hard
-        eggType = sender.currentTitle!
-        print("Starting timer for \(eggType)")
-        counter = eggTimes[hardness]!
-        progressBar.progress = 0.0
-        secondsPassed = 0
-        titleLabel.text = hardness
+        if progressBar.progress == 0 //fixes bug where you can start two Timers at the same time!
+        { let hardness = sender.currentTitle! // returns; Soft, Medium or Hard
+            timer.invalidate() //invalidate timer before it starts a new one on pressing button
+            titleLabel.font = .boldSystemFont(ofSize: 50)
+            eggType = sender.currentTitle!
+            print("Starting timer for \(eggType)")
+            totalTime = eggTimes[hardness]!
+            progressBar.setProgress(0, animated: true) //set progress back to zero
+            secondsPassed = 0
+            titleLabel.font = .boldSystemFont(ofSize: 30)
+            titleLabel.text = "\(hardness) eggs coming up!"
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+            
+        }
+        else
+        {
+            print("Player is playing")
+        }
         
+        
+    }
+    // *********************** AFTER USER SELECTS HARDNESS (COUNTER GOING DOWN) **********************
+    @objc func updateCounter() {
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            
+            progressBar.setProgress(Float(secondsPassed) / Float(totalTime), animated: true)
+            
+            print(Float(secondsPassed) / Float(totalTime))
+            
+            timeLeft.isHidden = false
+            
+            //      timeLeft.text = String((totalTime) - (secondsPassed))
+            //UPDATE UI LAbel with minutes and seconds!!!
+            let i = ((totalTime) - (secondsPassed))
+            timeLeft.text = String(timeString(time: TimeInterval(i))) //isnt upda
+        }
+        
+        else {
+            // ********************** WHEN EGGS ARE READY (REMANINING TIME 0) **********************
+            timer.invalidate()
+            titleLabel.font = .boldSystemFont(ofSize: 30)
+            titleLabel.text = String("Egg's are Ready!!!")
+            playSound(soundName: "alarm_sound")
+            stopTimerButton.isHidden = false
+            timeLeft.text = ""
+            timeLeft.font = .boldSystemFont(ofSize: 40)
+            
+            //FLASH ANIMATION WHEN EGGS ARE READY
+            UIView.animate(withDuration: 0.5, delay: 0.25, options: [.repeat, .autoreverse], animations: {
+                self.titleLabel.alpha = 0
+            }, completion: nil)
+        }
+    }
+    
+    
+    // ********************** SOUND SETTINGS **********************
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        self.player = try! AVAudioPlayer(contentsOf: url!)
+        self.player.play()
+        self.player.numberOfLoops = -1
+        
+    }
+    
+    func stopSound() {
+        player.stop()
+    }
+    // ********************** DISPLAYED TIME LEFT (CONVERTION) **********************
+    func timeString(time: TimeInterval) -> String {
+        //            let hour = Int(time) / 3600
+        let minute = Int(time) / 60 % 60
+        let second = Int(time) % 60
+        
+        // return formated string
+        return String(format: "%02i:%02i", minute, second)
+    }
+    
+    // ********************** WHEN USER PRESSES STOP TIMER BUTTON **********************
+    @IBAction func stopTimer(_ sender: UIButton) {
+        stopSound() //stops sound
+        //returns back to starting point
+        stopTimerButton.isHidden = true
+        progressBar.setProgress(0, animated: true)
+        secondsPassed = 0
+        titleLabel.text = "How do you like your eggs?"
+        timeLeft.isHidden = true
+        titleLabel.alpha = 1
     }
     
     
 }
+
